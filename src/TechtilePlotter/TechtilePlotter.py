b5f5415c948b27a13bd898aa54fedf6cd66a2bb3
Gilles Callebaut <callebaut.gilles@gmail.com>
2024-02-28 13:06:51 +0100

    Update TechtilePlotter.py

--- src/TechtilePlotter/TechtilePlotter.py
+++ src/TechtilePlotter/TechtilePlotter.py
@@ -1,27 +1,33 @@
 import plotly.graph_objects as go
 import numpy as np
+import os
+
 
 class TechtilePlotter:
 
     def __init__(self):
         # load positions from YAML
         # Create a layout with a specified aspect ratio
-           
+
         layout = go.Layout(
-                scene=dict(
-                    aspectmode='manual',
-                    aspectratio=dict(x=8.4, y=4, z=2.4),  # Adjust x and y ratio to make it rectangular
-                    xaxis=dict(range=[0, 8.4]),  # Set the range for the x-axis
-                    yaxis=dict(range=[0, 4]),  # Set the range for the y-axis
-                    zaxis=dict(range=[0, 2.4])   # Set the range for the z-axis
-                )
+            scene=dict(
+                aspectmode='manual',
+                # Adjust x and y ratio to make it rectangular
+                aspectratio=dict(x=8.4, y=4, z=2.4),
+                xaxis=dict(range=[0, 8.4]),  # Set the range for the x-axis
+                yaxis=dict(range=[0, 4]),  # Set the range for the y-axis
+                zaxis=dict(range=[0, 2.4])   # Set the range for the z-axis
             )
+        )
         self.fig = go.Figure(layout=layout)
         self.antennas_plotted = False
-        self.antennas_pos = [0] # placeholder to test
+        import yaml
+        with open(os.path.join(os.path.dirname(__file__), "..", "..", "positions.yml"), 'r') as file:
+            positions = yaml.safe_load(file)
+        self.antennas_descr = positions["antennes"]  # placeholder to test
         self.fig.add_trace(go.Scatter3d(x=(-1,),
-            y=(-1,),
-            z=(1,),showlegend=False))
+                                        y=(-1,),
+                                        z=(1,), showlegend=False))
 
     def microphones(self, pattern=False, directivity=False):
         """ Plots the microphones locations
@@ -32,7 +38,7 @@ class TechtilePlotter:
         """
         pass
 
-    def antennas(self, active_tiles:list=None, pattern=False, directivity=False):
+    def antennas(self, active_tiles: list = None, pattern=False, directivity=False):
         """ Plots the antenna locations
 
         Args:
@@ -40,18 +46,18 @@ class TechtilePlotter:
             pattern (bool, optional): Plot the pattern of the antennas (uses the normal of the tiles). Defaults to False.
             directivity (bool, optional): Plot the directivity of the antennas (uses the normal of the tiles). Defaults to False.
         """
-        #TODO plot based on normal (include real dimensions)
+        # TODO plot based on normal (include real dimensions)
         if not self.antennas_plotted:
             scale = 0.1
-            x_vals = np.array([0,0,1*scale,1*scale,0])
-            y_vals = np.array([0,0,0,0,0])
-            z_vals = np.array([0,1*scale,1*scale,0,0])
-            for ant in self.antennas_pos:
-                self.fig.add_trace(go.Scatter3d(x=x_vals+4, y=y_vals, z=z_vals+1, mode='lines', surfaceaxis=1, # add a surface axis ('1' refers to axes[1] i.e. the y-axis)
-            surfacecolor='#66c2a5',showlegend=False))
+            x_vals = np.array([0, 0, 1*scale, 1*scale, 0])
+            y_vals = np.array([0, 0, 0, 0, 0])
+            z_vals = np.array([0, 1*scale, 1*scale, 0, 0])
+            for ant in self.antennas_descr:
+                self.fig.add_trace(go.Scatter3d(x=x_vals+ant["x"], y=y_vals+ant["y"], z=z_vals+ant["z"], mode='lines', surfaceaxis=1,  # add a surface axis ('1' refers to axes[1] i.e. the y-axis)
+                                                surfacecolor='#66c2a5', showlegend=False))
             self.antennas_plotted = True
 
-    def measurements(self, x,y,z, values, color=None, label=None):
+    def measurements(self, x, y, z, values, color=None, label=None):
         if color is None:
             color = values
         self.fig.add_trace(go.Scatter3d(
@@ -60,7 +66,8 @@ class TechtilePlotter:
             z=z,
             text=label,
             mode='markers',
-            marker=dict(color=color,colorscale='Viridis', size=10, colorbar=dict(thickness=20))
+            marker=dict(color=color, colorscale='Viridis',
+                        size=10, colorbar=dict(thickness=20))
         ))
         pass
 
