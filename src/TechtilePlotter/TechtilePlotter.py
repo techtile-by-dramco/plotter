67fedeadfbbe79973015bc544525a244e84ddf2b
Gilles Callebaut <callebaut.gilles@gmail.com>
2025-06-24 13:56:50 +0200

    Update TechtilePlotter.py

--- src/TechtilePlotter/TechtilePlotter.py
+++ src/TechtilePlotter/TechtilePlotter.py
@@ -215,7 +215,7 @@ class TechtilePlotter:
         # Run the Dash app in a separate thread
         kwargs = {"debug": False, "port": 8080, "host": "0.0.0.0"}
         self.thr = threading.Thread(
-            target=self.app.run_server, daemon=True, kwargs=kwargs
+            target=self.app.run, daemon=True, kwargs=kwargs
         )
         self.thr.start()
 
